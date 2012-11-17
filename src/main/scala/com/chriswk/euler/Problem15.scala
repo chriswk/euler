4b9c9a6a999ec2ac531ee75ffe0c78f99b65bcea
Christopher Kolstad <christopher.kolstad@gmail.com>
2012-11-17 20:27:02 +0100

    Opp15 oppgave og profiling

--- src/main/scala/com/chriswk/euler/Problem15.scala
+++ src/main/scala/com/chriswk/euler/Problem15.scala
@@ -1,5 +1,11 @@
 package com.chriswk.euler
 
+import com.chriswk.common.Profiling
+
+/**
+  * Starting in the top left corner of a 22 grid, there are 6 routes (without backtracking) to the bottom right corner.
+  * How many routes are there through a 2020 grid?
+  */
 object Problem15 extends App {
 
   def f(row: Seq[Long], c: Int): Long = {
@@ -10,7 +16,7 @@ object Problem15 extends App {
     if (c == 0) next.last else f(next, c - 1)
   }
 
-  def answer(n: Int) = f(List.fill(n + 1)(1L), n - 1)
-
+  def answer(n: Int) = Profiling.timed(Profiling.printTime("Found routes for " +n +"x"+n+" grid")) { f(List.fill(n + 1)(1L), n - 1) }
+  assert(answer(2) == 6)
   println(answer(20))
 }
