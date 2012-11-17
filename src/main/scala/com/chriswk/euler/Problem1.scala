3a7cd2ae6d9e9f5d38561118296c0b5a657b578f
Christopher Kolstad <christopher.kolstad@gmail.com>
2012-11-17 23:01:07 +0100

    Abuse static methods....

--- src/main/scala/com/chriswk/euler/Problem1.scala
+++ src/main/scala/com/chriswk/euler/Problem1.scala
@@ -1,6 +1,7 @@
 package com.chriswk.euler
 
-import com.chriswk.common.{MathHelpers, Profiling}
+import com.chriswk.common.MathHelpers._
+import com.chriswk.common.Profiling._
 /**
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
  * Find the sum of all the multiples of 3 or 5 below 1000.
@@ -16,21 +17,21 @@ object Problem1 extends App {
       case head :: rest => {
         val n = limit / head // # of elements in series to sum
         head * (n * (n + 1) / 2) -
-          gaussianWay(limit, priorInts.map(MathHelpers.lcm(head, _)).filter(_ <= limit)) +
+          gaussianWay(limit, priorInts.map(lcm(head, _)).filter(_ <= limit)) +
           gaussianWay(limit, rest, head :: priorInts)
       }
     }
   }
 
   val thousand = 1000
-  Profiling.timed(Profiling.printTime("With view filter method (1000,3,5)")) {
+  timed(printTime("With view filter method (1000,3,5)")) {
     println(sumOfNumbers(thousand, List(3, 5)))
   }
-  Profiling.timed(Profiling.printTime("With gaussian method (1000,3,5)")) {
+  timed(printTime("With gaussian method (1000,3,5)")) {
 
     println(gaussianWay(thousand - 1, List(3, 5)))
   }
-  Profiling.timed(Profiling.printTime("With gaussian method (1000000,3,5,11,13)")) {
+  timed(printTime("With gaussian method (1000000,3,5,11,13)")) {
     val tenZotta = BigInt("10000000000000000000000000")
     println(gaussianWay(tenZotta - 1, List(3,5,7,9)))
   }
