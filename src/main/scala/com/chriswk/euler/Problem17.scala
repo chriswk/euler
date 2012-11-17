62a14cb6af8e9fbab9e940abca878ad199c3fcf4
Christopher Kolstad <christopher.kolstad@gmail.com>
2012-11-17 20:30:16 +0100

    Profliing Problem17

--- src/main/scala/com/chriswk/euler/Problem17.scala
+++ src/main/scala/com/chriswk/euler/Problem17.scala
@@ -1,11 +1,11 @@
 package com.chriswk.euler
+
+import com.chriswk.common.Profiling
+
 /***
-If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
-
-If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
-
-
-NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+  * If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+  * If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+  * NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
   */
 
 object Problem17 extends App {
@@ -20,6 +20,6 @@ object Problem17 extends App {
     case 1000 => 11
   }
 
-  val answer = (1 to 1000).map(name).sum
+  val answer = Profiling.timed(Profiling.printTime("Numbers to letters in ")) { (1 to 1000).map(name).sum }
   print(answer)
 }
