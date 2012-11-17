359011559ac23172f0246a2dc8ae7ac44fae766e
Christopher Kolstad <christopher.kolstad@gmail.com>
2012-11-17 18:39:34 +0100

    Math Rocks

--- src/main/scala/com/chriswk/euler/Problem1.scala
+++ src/main/scala/com/chriswk/euler/Problem1.scala
@@ -4,30 +4,29 @@ import com.chriswk.common.{MathHelpers, Profiling}
 
 object Problem1 extends App {
   def sumOfNumbers(limit: Int, filters: List[Int]) = {
-    (1 until limit).view.filter(n => filters.exists(p => n % p == 0))
+    (1 until limit).view.filter(n => filters.exists(p => n % p == 0)).sum
   }
+
   def gaussianWay(limit: BigInt, filters: List[BigInt], priorInts: List[BigInt] = Nil): BigInt = {
     filters match {
       case Nil => 0
-      case int::rest =>
-        val n = limit / int // # of elements in series to sum
-        int * ( n * (n+1) / 2) -
-          gaussianWay(limit, priorInts.map(MathHelpers.lcm(int,_)).filter(_<=limit)) +
-          gaussianWay(limit, rest, int::priorInts)
+      case head :: rest => {
+        val n = limit / head // # of elements in series to sum
+        head * (n * (n + 1) / 2) -
+          gaussianWay(limit, priorInts.map(MathHelpers.lcm(head, _)).filter(_ <= limit)) +
+          gaussianWay(limit, rest, head :: priorInts)
+      }
     }
   }
 
   Profiling.timed(Profiling.printTime("With view filter method (1000,3,5)")) {
-    sumOfNumbers(1000, List(3, 5))
+    println(sumOfNumbers(1000, List(3, 5)))
   }
   Profiling.timed(Profiling.printTime("With gaussian method (1000,3,5)")) {
-    gaussianWay(1000, List(3, 5))
-  }
-  Profiling.timed(Profiling.printTime("With view filter method (1000000,3,5,11,13)")) {
-    sumOfNumbers(1000000, List(3,5,11,13))
+    println(gaussianWay(999, List(3, 5)))
   }
   Profiling.timed(Profiling.printTime("With gaussian method (1000000,3,5,11,13)")) {
-    gaussianWay(1000000, List(3,5,11,13))
+    val init = BigInt("10000000000000000000000000")
+    println(gaussianWay(init - 1, List(3,5,7,9)))
   }
-
 }
