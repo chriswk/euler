dea99566c4118969a957473afd141b5a8e9a950a
Christopher Kolstad <christopher.kolstad@gmail.com>
2012-11-18 21:23:57 +0100

    Problem32 with for comprehensions

---
+++ src/main/scala/com/chriswk/euler/Problem32.scala
@@ -0,0 +1,16 @@
+package com.chriswk.euler
+
+/***
+  * We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+  * The product 7254 is unusual, as the identity, 39  186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+  * Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+  * HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.*/
+object Problem32 extends App {
+  val pandigital = for {
+    a <- 2 to 10000; b <- 2 to 10000 / a
+    m = a * b; s = a.toString + b + m
+    if s.length == 9 && (1 to 9).mkString.forall(s.contains(_))
+  } yield m
+  val answer = pandigital.distinct.sum
+  println(answer)
+}
